8da47a9dfb4bbc2eb739339e8280ff81d78f8cf8
PedTorSes <pedtorses@alu.edu.gva.es>
2026-03-03 21:44:42 +0100

    Adición de los comentarios a la clase

--- src/main/java/com/ieschabas/Ejercicio02_Agenda.java
+++ src/main/java/com/ieschabas/Ejercicio02_Agenda.java
@@ -1,4 +1,9 @@
 package com.ieschabas;
+/**
+ * Clase Agenda en la cual creamos el archivo "agenda.txt" y escribimos los datos introducidos por el usuario
+ * @author Pedro Enrique Torres Sesé
+ * @version 1.0
+ */
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -7,26 +12,33 @@ import java.util.Scanner;
 
 public class Ejercicio02_Agenda {
     static void main(String[] args) {
+        //Creamos el scanner para aceptar la introducción de datos
         Scanner scanner = new Scanner(System.in);
 
+        //Creamos el archivo "agenda.txt"
         try (BufferedWriter bw = new BufferedWriter(new FileWriter("Agenda.txt"))) {
 
+            //Establecemos la condición por la cual el usuario saldrá de la pantalla
+            //Guardamos el nombre
             while (true) {
                 System.out.println("Introduce nombre (Salir = intro con nombre vacío): ");
                 String nombre = scanner.next();
-
                 if (nombre.isEmpty()) {
                     break;
                 }
+
+                //Guardamos el número de teléfono
                 System.out.println("Introduce su número de teléfono: ");
                 String telefono = scanner.next();
 
+                //Establecemos el formato con el cual guardaremos los contactos y teléfonos
                 bw.write(nombre + " - " + telefono);
                 bw.newLine();
                 System.out.println("Contacto: " + nombre);
             }
             System.out.println("Creación correcta");
 
+            //Creamos el mensaje de error en caso de no poder crearse el contacto
         } catch (IOException e) {
             System.out.println("Ha habido un error en la cración de contacto: " + e.getMessage());;
         }
